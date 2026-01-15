b5046396875923d3d1838f1511486a3d55593848
Shumon Khan <codewithshumon@gmail.com>
2026-01-15 18:13:08 +0600

    fix css issue

--- apps/admin/src/pages/Dashboard.tsx
+++ apps/admin/src/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard: React.FC = () => {
     return (
         <div className=" text-2xl">
-            <h1 className=" to-red-500">Dashboard</h1>
+            <h1 className="text-red-500">Dashboard</h1>
             <p>Welcome to the admin dashboard!</p>
         </div>
     );
